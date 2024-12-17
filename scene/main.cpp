3bb1f60752709cd5f8157084db70cb0b99c2de8e
Adrian Mincu <adrian-lucian.mincu@s.unibuc.ro>
2024-12-17 12:14:29 +0200

    added template files

---
+++ scene/main.cpp
@@ -0,0 +1,122 @@
+#include <windows.h>  // biblioteci care urmeaza sa fie incluse
+#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <stdio.h>
+#include <GL/glew.h> // glew apare inainte de freeglut
+#include <GL/freeglut.h> // nu trebuie uitat freeglut.h
+#include "loadShaders.h"
+
+
+GLuint
+	VaoId,
+	VboId,
+	ColorBufferId,
+	ProgramId;
+
+
+void CreateVBO(void)
+{
+	GLfloat Vertices[] = {
+		0.5f,  0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f,
+		0.5f,  0.5f, 0.0f, 1.0f
+	};
+	
+	GLfloat Colors[] = {
+	  1.0f, 0.5f, 0.2f, 1.0f,
+	  1.0f, 0.5f, 0.2f, 1.0f,
+	  1.0f, 0.5f, 0.2f, 1.0f,
+	  1.0f, 0.5f, 0.2f, 1.0f,
+	  1.0f, 0.5f, 0.2f, 1.0f,
+	  1.0f, 0.5f, 0.2f, 1.0f,
+	};
+	
+	glGenBuffers(1, &VboId);
+	glBindBuffer(GL_ARRAY_BUFFER, VboId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VaoId);
+	glBindVertexArray(VaoId);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glGenBuffers(1, &ColorBufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+	
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+}
+
+
+void DestroyVBO(void)
+{
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &ColorBufferId);
+	glDeleteBuffers(1, &VboId);
+
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VaoId);
+}
+
+
+void CreateShaders(void)
+{
+	ProgramId = LoadShaders("shader.vert", "shader.frag");
+	glUseProgram(ProgramId);
+}
+
+
+void DestroyShaders(void)
+{
+	glDeleteProgram(ProgramId);
+}
+
+
+void Initialize(void)
+{
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // culoarea de fond a ecranului
+	CreateVBO();
+	CreateShaders();
+}
+
+
+void RenderFunction(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);       
+	
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 3, 3);
+
+	glFlush();
+}
+
+
+void Cleanup(void)
+{
+	DestroyShaders();
+	DestroyVBO();
+}
+
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("3D Car Scene");
+	
+	glewInit();
+	Initialize();
+	
+	glutDisplayFunc(RenderFunction);
+	glutCloseFunc(Cleanup);
+	glutMainLoop();
+}
+
---
+++ scene/shader.vert
@@ -0,0 +1,13 @@
+// Shader-ul de varfuri / Vertex shader
+#version 330 core
+
+layout (location = 0) in vec4 in_Position;
+layout (location = 1) in vec4 in_Color;
+
+out vec4 ex_Color;
+
+void main ()
+{
+   gl_Position = in_Position;
+   ex_Color = in_Color;
+}
